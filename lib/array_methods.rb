edb06f693c68bfb2af4b06eeef03d869d44b331e
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-28 00:39:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
 array.length.times do |i|
   if array.include?(value_to_find)
-    return i[]
+    return array.index(value_to_find)
   else
     return nil
   end
